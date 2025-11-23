9e99ecb3fbda0c6c980962fa12364173d08207f6
meShahabaj <shahabaj773@gmail.com>
2025-11-23 13:44:52 +0530

    same

--- Backend/app.py
+++ Backend/app.py
@@ -41,5 +41,5 @@ app.include_router(rps_router)
 if __name__ == "__main__":
     import uvicorn
 
-    port = int(os.getenv("PORT"))
-    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
