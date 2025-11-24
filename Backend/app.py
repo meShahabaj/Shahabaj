d9880428a19cd9f45a107db24035031cb0369f04
meShahabaj <shahabaj773@gmail.com>
2025-11-25 03:44:54 +0530

    test

--- Backend/app.py
+++ Backend/app.py
@@ -1,15 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from router import (
-    test_router,
-    image_editor_router,
-    salary_predictor_router,
-    chatbot_router,
-    face_extractor_router,
-    rps_router,
-    number_identifier_router,
-)
+# from router import (
+#     test_router,
+#     image_editor_router,
+#     salary_predictor_router,
+#     chatbot_router,
+#     face_extractor_router,
+#     rps_router,
+#     number_identifier_router,
+# )
 import os
 
 app = FastAPI()
@@ -34,12 +34,12 @@ for route in app.routes:
 
 #------- REGISTER ROUTES ------------#
 app.include_router(test_router)
-app.include_router(image_editor_router)
-app.include_router(face_extractor_router)
-app.include_router(salary_predictor_router)
-app.include_router(chatbot_router)
-app.include_router(number_identifier_router)
-app.include_router(rps_router)
+# app.include_router(image_editor_router)
+# app.include_router(face_extractor_router)
+# app.include_router(salary_predictor_router)
+# app.include_router(chatbot_router)
+# app.include_router(number_identifier_router)
+# app.include_router(rps_router)
 
 if __name__ == "__main__":  
     port = int(os.getenv("PORT", 8000))
